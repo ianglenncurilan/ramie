6ca67d447eb1becda0c79383a6d8a2c0e1d168c1
Ian Glenn Curilan <iancurilan1027.com>
2025-09-19 11:48:19 +0800

    add: .env and supabasejs

---
+++ src/services/supabase.js
@@ -0,0 +1,25 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY,
+)
+
+// Define and export formActionDefault
+export const formActionDefault = {
+  formProcess: false,
+  formStatus: 200,
+  formErrorMessage: '',
+  formSuccessMessage: '',
+}
+
+export const isAuthenticated = async () => {
+  const { data, error } = await supabase.auth.getSession()
+  if (error) {
+    console.error('Error getting session:', error.message)
+    return false
+  }
+
+  return !data.session
+}
